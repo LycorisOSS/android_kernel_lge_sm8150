78d97b06cb707b7125e65cb86c95d73ede8ea516
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2018-01-31 20:24:15 -0800

    rcu: Make expedited RCU CPU selection avoid unnecessary stores
    This commit reworks the first loop in sync_rcu_exp_select_cpus()
    to avoid doing unnecssary stores to other CPUs' rcu_data
    structures.  This speeds up that first loop by roughly a factor of
    two on an old x86 system.  In the case where the system is mostly
    idle, this loop incurs a large fraction of the overhead of the
    synchronize_rcu_expedited().  There is less benefit on busy systems
    because the overhead of the smp_call_function_single() in the second
    loop dominates in that case.
    
    However, it is not unusual to do configuration chances involving
    RCU grace periods (both expedited and normal) while the system is
    mostly idle, so this optimization is worth doing.
    
    While we are in the area, this commit also adds parentheses to arguments
    used by the for_each_leaf_node_possible_cpu() macro.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/rcu/tree_exp.h
+++ kernel/rcu/tree_exp.h
@@ -463,17 +463,26 @@ static void sync_rcu_exp_select_cpus(struct rcu_state *rsp,
 	sync_exp_reset_tree(rsp);
 	trace_rcu_exp_grace_period(rsp->name, rcu_exp_gp_seq_endval(rsp), TPS("select"));
 	rcu_for_each_leaf_node(rsp, rnp) {
-		rnp->exp_need_flush = false;
-		if (!READ_ONCE(rnp->expmask))
-			continue; /* Avoid early boot non-existent wq. */
-		rnp->rew.rew_func = func;
-		rnp->rew.rew_rsp = rsp;
-		if (!READ_ONCE(rcu_par_gp_wq) ||
-		    rcu_scheduler_active != RCU_SCHEDULER_RUNNING ||
-		    rcu_is_last_leaf_node(rsp, rnp)) {
-			/* No workqueues yet or last leaf, do direct call. */
-			sync_rcu_exp_select_node_cpus(&rnp->rew.rew_work);
-			continue;
+		raw_spin_lock_irqsave_rcu_node(rnp, flags);
+
+		/* Each pass checks a CPU for identity, offline, and idle. */
+		mask_ofl_test = 0;
+		for_each_leaf_node_cpu_mask(rnp, cpu, rnp->expmask) {
+			unsigned long mask = leaf_node_cpu_bit(rnp, cpu);
+			struct rcu_data *rdp = per_cpu_ptr(rsp->rda, cpu);
+			struct rcu_dynticks *rdtp = per_cpu_ptr(&rcu_dynticks, cpu);
+			int snap;
+
+			if (raw_smp_processor_id() == cpu ||
+			    !(rnp->qsmaskinitnext & mask)) {
+				mask_ofl_test |= mask;
+			} else {
+				snap = rcu_dynticks_snap(rdtp);
+				if (rcu_dynticks_in_eqs(snap))
+					mask_ofl_test |= mask;
+				else
+					rdp->exp_dynticks_snap = snap;
+			}
 		}
 		mask_ofl_ipi = rnp->expmask & ~mask_ofl_test;
 
@@ -487,7 +496,7 @@ static void sync_rcu_exp_select_cpus(struct rcu_state *rsp,
 		raw_spin_unlock_irqrestore_rcu_node(rnp, flags);
 
 		/* IPI the remaining CPUs for expedited quiescent state. */
-		for_each_leaf_node_possible_cpu(rnp, cpu) {
+		for_each_leaf_node_cpu_mask(rnp, cpu, rnp->expmask) {
 			unsigned long mask = leaf_node_cpu_bit(rnp, cpu);
 			struct rcu_data *rdp = per_cpu_ptr(rsp->rda, cpu);
 
